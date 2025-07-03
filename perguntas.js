715a7309cc7ac1bdb334d6eff4644a6a879ba0fe
Aline J Rodrigues <jesus.rodrigues.aline@escola.pr.gov.br>
2025-07-03 15:16:05 -0300

    function

--- perguntas.js
+++ perguntas.js
@@ -1,16 +1,16 @@
-criaCartao(Categria, pergunta, resposta){
-‘Programação’,
-‘O que é JavaScript?’,
-‘O JavaScript é uma linguagem de programação’
+function criaCartao(Categria, pergunta, resposta){
+    console.log(categoria, pergunta, resposta);
+//‘Programação’,
+//‘O que é JavaScript?’,
+//‘O JavaScript é uma linguagem de programação’
+
+//‘Programação’,
+//‘O que é uma função?’,
+//‘Uma função é um bloco de código que executa alguma tarefa’
+
+//‘Geografia’,
+//‘Qual a capital da França?’,
+//‘A capital da França é Paris’
 }
 
-criaCartao(
-‘Geografia’,
-‘Qual a capital da França?’,
-‘A capital da França é Paris’
-)
-criaCartao(
-‘Programação’,
-‘O que é uma função?’,
-‘Uma função é um bloco de código que executa alguma tarefa’
-)
+
