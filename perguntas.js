1a05ecbd2bfb354a4f892e2a60dc445107643fce
Aline J Rodrigues <jesus.rodrigues.aline@escola.pr.gov.br>
2025-07-03 15:27:12 -0300

    vfer

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
 
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
 
 function criaCartao(Categoria, pergunta, resposta){
     console.log(Categoria, pergunta, resposta);
 }
-'Programação',
-"O que é JavaScript?",
-"O JavaScript é uma linguagem de programação"
 
-"Programação",
-"O que é uma função?",
-"Uma função é um bloco de código que executa alguma tarefa"
-
-"Geografia",
-"Qual a capital da França?",
-"A capital da França é Paris"
 
