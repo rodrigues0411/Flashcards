967438e9e45ebdb8e94faf9eef5539db29a4357a
Aline J Rodrigues <jesus.rodrigues.aline@escola.pr.gov.br>
2025-07-03 15:18:26 -0300

    ""

--- perguntas.js
+++ perguntas.js
@@ -1,16 +1,16 @@
 function criaCartao(Categria, pergunta, resposta){
     console.log(categoria, pergunta, resposta);
-//‘Programação’,
-//‘O que é JavaScript?’,
-//‘O JavaScript é uma linguagem de programação’
+'Programação',
+"O que é JavaScript?",
+"O JavaScript é uma linguagem de programação"
 
-//‘Programação’,
-//‘O que é uma função?’,
-//‘Uma função é um bloco de código que executa alguma tarefa’
+"Programação",
+"O que é uma função?",
+"Uma função é um bloco de código que executa alguma tarefa"
 
-//‘Geografia’,
-//‘Qual a capital da França?’,
-//‘A capital da França é Paris’
+"Geografia",
+"Qual a capital da França?",
+"A capital da França é Paris"
 }
 
 
